69e45c62193d0eab79fbb8ed90d866675b19054a
CrazyWillBear <crazywillbear@gmail.com>
2022-07-09 21:37:18 -0800

    First real commit

--- src/main/java/net/capbear/queue/Queue.java
+++ src/main/java/net/capbear/queue/Queue.java
@@ -1,5 +1,9 @@
 package net.capbear.queue;
 
+import net.md_5.bungee.api.ChatColor;
+import net.md_5.bungee.api.ProxyServer;
+import net.md_5.bungee.api.Title;
+import net.md_5.bungee.api.chat.ComponentBuilder;
 import net.md_5.bungee.api.config.ServerInfo;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.md_5.bungee.api.event.PlayerDisconnectEvent;
@@ -13,11 +17,12 @@ import java.util.Vector;
 import java.util.concurrent.TimeUnit;
 
 public final class Queue extends Plugin implements Listener {
-    private static final String queueServerName = "hub"; // replace hub as needed
-    private static final String destinationServerName = "survival"; // replace survival as needed
-    private static final int maxPlayersDestinationServer = 0;
-    Vector<ProxiedPlayer> queue;
+    // configurable variables
+    private static final String queueServerName = "hub"; // replace with the name of your queue server
+    private static final String destinationServerName = "survival"; // replace with the name of your destination server
+    private static final int maxPlayersDestinationServer = 100; // replace with the maximum number of players allowed on your destination server
 
+    Vector<ProxiedPlayer> queue; // non-configurable, leave as-is
     @Override
     public void onEnable() {
         // Plugin startup logic
@@ -31,30 +36,32 @@ public final class Queue extends Plugin implements Listener {
             public void run() {
                 System.out.println("Players on " + destinationServerName + ": " + getProxy().getServerInfo(destinationServerName).getPlayers().size() + "\t|\tQueue Size: " + queue.size());
                 for (ProxiedPlayer player : getProxy().getServerInfo(queueServerName).getPlayers()) {
-                    player.sendMessage("§3You are currently in position " + (queue.indexOf(player) + 1) + "/" + queue.size());
-                }
+                    ComponentBuilder builder = new ComponentBuilder();
+                    builder.append("You are currently in position " + (queue.indexOf(player) + 1) + "/" + (queue.size())).color(ChatColor.AQUA);
+                    Title title = ProxyServer.getInstance().createTitle().title(builder.create()).fadeOut(100).fadeIn(0);
+                    title.send(player);
+                } // send each player a title containing queue info
                 if (getProxy().getServerInfo(destinationServerName).getPlayers().size() < maxPlayersDestinationServer && queue.size() > 0) {
                     ServerInfo destination = getProxy().getServerInfo(destinationServerName);
                     queue.get(0).connect(destination);
                     queue.remove(0);
-                }
+                } // if at front of line and slot is open, send to main server
             }
-        }, 5, 5, TimeUnit.SECONDS);
+        }, 5, 5, TimeUnit.SECONDS); // repeat forever every 5 seconds
     }
 
     @EventHandler
     public void onPostLogin(PostLoginEvent event) {
         // this assumes player is forced to join queue server
         queue.add(event.getPlayer());
-    }
+    } // add player to queue upon login
 
     @EventHandler
     public void onPlayerDisconnect(PlayerDisconnectEvent event) {
-        System.out.println("Player left server " + event.getPlayer().getServer().getInfo().getName());
         if (event.getPlayer().getServer().getInfo().getName().equals(queueServerName)) {
             queue.remove(event.getPlayer());
         }
-    }
+    } // remove player from queue if they disconnect from queue server
 
     @Override
     public void onDisable() {
